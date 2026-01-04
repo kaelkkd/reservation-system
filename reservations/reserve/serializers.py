d2304ad4c0653d4f991655c332f8244c9b780274
kael <kaelsiebra233@gmail.com>
2026-01-04 11:58:00 -0300

    feat: add reservation update serializer

--- reservations/reserve/serializers.py
+++ reservations/reserve/serializers.py
@@ -1,5 +1,6 @@
 from rest_framework import serializers
 from .models import Location, Reservation
+from .mixins import ReservationValidationMixin
 
 class LocationSerializer(serializers.ModelSerializer):
     address = serializers.CharField(source="address_line")
@@ -18,7 +19,7 @@ class LocationSerializer(serializers.ModelSerializer):
             raise serializers.ValidationError({"capacity": "The capacity must be at least 1."})
         return value
     
-class ReservationSerializer(serializers.ModelSerializer):
+class ReservationSerializer(ReservationValidationMixin, serializers.ModelSerializer):
     reservation_id = serializers.UUIDField(read_only=True)
     reserved_at = serializers.CharField(source="created_at", read_only=True)
     location = LocationSerializer(read_only=True)
@@ -48,20 +49,42 @@ class ReservationSerializer(serializers.ModelSerializer):
         start = attrs.get("start_date")
         end = attrs.get("end_date")
         num_people = attrs.get("num_people")
-
-        if start >= end:
-            raise serializers.ValidationError({"end_date": "End date must be after start date."})  
-        
-        overlaps = Reservation.objects.filter(location=location, start_date__lt=end, end_date__gt=start,).exists()
-        if overlaps > 0:
+        self.validate_dates(start, end)
+        self.validate_capacity(num_people, location)
+        if self.validate_overlap(location, start, end):
             raise serializers.ValidationError({"non_fiel_errors": "Location not available at the selected date."})
-        
-        if num_people > location.capacity:
-            raise serializers.ValidationError({"number_of_people": "The location cannot acommodate that many people."})
         
         return attrs      
 
+class UpdateReservationSerializer(ReservationValidationMixin, serializers.ModelSerializer):
+    reservation_id = serializers.UUIDField(read_only=True)
+    reserved_at = serializers.CharField(source="created_at", read_only=True)
+    location = LocationSerializer(read_only=False)
+    location_id = serializers.PrimaryKeyRelatedField(queryset=Location.objects.all(), source="location", read_only=True)
+    number_of_people = serializers.IntegerField(source="num_people")
+    user = serializers.PrimaryKeyRelatedField(source="reserved_by", read_only=True)
 
-
-
+    class Meta:
+        model = Reservation
+        fields = (
+            'reservation_id',
+            'user',
+            'location',
+            'location_id',
+            'start_date',
+            'end_date',
+            'reserved_at',
+            'number_of_people'
+        )
     
+    def validate(self, attrs):
+        location = attrs.get("location")
+        start = attrs.get("start_date")
+        end = attrs.get("end_date")
+        num_people = attrs.get("num_people")
+        self.validate_dates(start, end)
+        self.validate_capacity(num_people, location)
+        if self.validate_overlap(location, start, end, exclude_id=self.instance.reservation_id):
+            raise serializers.ValidationError({"non_fiel_errors": "Location not available at the selected date."})
+        
+        return attrs
