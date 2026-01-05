96c45181b07b73dcf6f245432f2ea1067fc83352
kael <kaelsiebra233@gmail.com>
2026-01-05 14:19:52 -0300

    refactor(reservation): convert reservation views into a viewset

--- reservations/reserve/views.py
+++ reservations/reserve/views.py
@@ -1,12 +1,12 @@
-from rest_framework import generics, filters, viewsets
+from rest_framework import viewsets
 from .models import Location, Reservation
 from .filters import LocationFilter
-from .serializers import LocationSerializer, ReservationSerializer
+from .serializers import LocationSerializer, ReservationSerializer, ReservationUpdateSerializer
 from rest_framework.permissions import IsAuthenticated, IsAdminUser, AllowAny
 from django_filters.rest_framework import DjangoFilterBackend
 from django.utils.decorators import method_decorator
 from django.views.decorators.cache import cache_page
-from .tasks import send_reservation_confirmation
+from .tasks import send_reservation_confirmation, send_cancellation_confirmation
 
 class LocationViewSet(viewsets.ModelViewSet):
     queryset = Location.objects.order_by('pk')
@@ -31,24 +31,24 @@ class LocationViewSet(viewsets.ModelViewSet):
 
         return super().get_permissions()
     
-class ReservationListCreateAPIView(generics.ListCreateAPIView):
-    serializer_class = ReservationSerializer
-    permission_classes = [IsAuthenticated]
-
-    def get_queryset(self):
-        user = self.request.user
-        queryset = Reservation.objects.select_related("location")
-        return queryset if user.is_staff else queryset.filter(reserved_by=user)
-            
-    def perform_create(self, serializer):
-        reservation = serializer.save(reserved_by=self.request.user)
-        send_reservation_confirmation.delay(reservation.reservation_id, self.request.user.email)
-    
-class ReservationDetailAPIView(generics.RetrieveDestroyAPIView):
-    serializer_class = ReservationSerializer
+class ReservationViewSet(viewsets.ModelViewSet):
     lookup_field = "reservation_id"
     lookup_url_kwarg = "reservation_id"
-
+    permission_classes = [IsAuthenticated]
+    
+    def get_serializer_class(self):
+        if self.action in ["update", "partial_update"]:
+            return ReservationUpdateSerializer
+        return ReservationSerializer
+        
     def get_queryset(self):
         user = self.request.user
         return Reservation.objects.all() if user.is_staff else Reservation.objects.filter(reserved_by=user)
+    
+    def perform_create(self, serializer):
+        reservation = serializer.save(reserved_by=self.request.user)
+        send_reservation_confirmation.delay(reservation.reservation_id, self.request.user.email)
+
+    def perform_destroy(self, instance):
+        send_cancellation_confirmation(instance.reservation_id, instance.reserved_by.email)
+        instance.delete()
