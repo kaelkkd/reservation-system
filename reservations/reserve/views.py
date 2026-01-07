fd70e1b9e972693782c08eb254cb0b16261d054b
kael <kaelsiebra233@gmail.com>
2026-01-07 13:51:39 -0300

    refactor(cache): caching done in the queryset

--- reservations/reserve/views.py
+++ reservations/reserve/views.py
@@ -5,6 +5,7 @@ from .serializers import LocationSerializer, ReservationSerializer, ReservationU
 from rest_framework.permissions import IsAuthenticated, IsAdminUser, AllowAny
 from django.utils.decorators import method_decorator
 from django.views.decorators.cache import cache_page
+from django.core.cache import cache
 from .tasks import send_reservation_confirmation, send_cancellation_confirmation
 
 class LocationViewSet(viewsets.ModelViewSet):
@@ -12,14 +13,16 @@ class LocationViewSet(viewsets.ModelViewSet):
     serializer_class = LocationSerializer
     filterset_class = LocationFilter
 
-    @method_decorator(cache_page(60 * 15, key_prefix='location_list'))
     def list(self, request, *args, **kwargs):
         return super().list(request, *args, **kwargs)
     
     def get_queryset(self):
-        import time
-        time.sleep(2)
-        return super().get_queryset()
+        qs = cache.get("locations_qs")
+        if qs is None:
+            qs = Location.objects.order_by("pk")
+            cache.set("locations_qs", qs, 60 * 15)
+        
+        return qs
 
     def get_permissions(self):
         if self.request.method in ["PUT", "PATCH", "DELETE"]:
